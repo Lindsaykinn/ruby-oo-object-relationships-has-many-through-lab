346393b9d661e0e3aef14f55056ec419820c6baf
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-11-26 12:21:46 -0500

    commit tree ea667d61e6e5a0b283cbcddf80deb31f07433535

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,27 @@
+class Appointment
+    attr_accessor :date, :doctor, :patient
 
+    @@all = []
+
+    def initialize(date, patient, doctor)
+        @date = date
+        @doctor = doctor        
+        @patient = patient       
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    #the below two methods were screwing up my tests. why?
+    # def patient
+    #     self.map {|appointment| appointment.patient}
+    # end
+
+    # def doctor
+    #     self.map do |a|
+    #         a.doctor
+    #     end
+    # end
+end
--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,30 @@
+class Artist
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    #new_song does not work without the #genres method
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+
+    end
+
+    def genres
+        songs.map do |song|
+            song.genre
+        end
+    end
+   
+end
